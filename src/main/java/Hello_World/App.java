374912682031603f8282f57b79b01bd8872b715f
T. Shailendra Nag <sailunag18@gmail.com>
2019-02-23 14:31:26 +0530

    Update App.java

--- src/main/java/Hello_World/App.java
+++ src/main/java/Hello_World/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! and Shailendra" );
+        System.out.println( "Hello World! and Shailendra Nag Tiru" );
     }
 }
